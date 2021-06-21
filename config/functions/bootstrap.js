f766dfab8ed31c7d5ea0d39a2f067f1b3c25ee0e
Jonathan <jonathan.sendiko@gmail.com>
2021-06-21 20:41:37 +0700

    Chat Prototype

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,13 +1,95 @@
-'use strict';
+"use strict";
+const {
+  findUser,
+  createUser,
+  deleteUser,
+  userExists,
+} = require("./utils/database");
 
-/**
- * An asynchronous bootstrap function that runs before
- * your application gets started.
- *
- * This gives you an opportunity to set up your data model,
- * run jobs, or perform some special logic.
- *
- * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
- */
+module.exports = () => {
+  var io = require("socket.io")(strapi.server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true,
+    },
+  });
+  io.on("connection", function (socket) {
+    socket.on("join", async ({ username, room }, callback) => {
+      try {
+        const userExists = await findUser(username, room);
 
-module.exports = () => {};
+        if (userExists.length > 0) {
+          callback(
+            `User ${username} already exists in room no${room}. Please select a different name or room`
+          );
+        } else {
+          const user = await createUser({
+            username: username,
+            room: room,
+            status: "ONLINE",
+            socketId: socket.id,
+          });
+
+          if (user) {
+            socket.join(user.room);
+            socket.emit("welcome", {
+              user: "bot",
+              text: `${user.username}, Welcome to room ${user.room}.`,
+              userData: user,
+            });
+            socket.broadcast.to(user.room).emit("message", {
+              user: "bot",
+              text: `${user.username} has joined`,
+            });
+          } else {
+            callback(`user could not be created. Try again!`);
+          }
+        }
+        callback();
+      } catch (err) {
+        console.log("Err occured, Try again!", err);
+      }
+    });
+    socket.on("sendMessage", async (data, callback) => {
+      try {
+        const user = await userExists(data.userId);
+        if (user) {
+          io.to(user.room).emit("message", {
+            user: user.username,
+            text: data.message,
+          });
+          io.to(user.room).emit("roomInfo", {
+            room: user.room,
+            users: await getUsersInRoom(user.room),
+          });
+        } else {
+          callback(`User doesn't exist in the database. Rejoin the chat`);
+        }
+        callback();
+      } catch (err) {
+        console.log("err inside catch block", err);
+      }
+    });
+    socket.on("disconnect", async (data) => {
+      try {
+        console.log("DISCONNECTED!!!!!!!!!!!!");
+        const user = await deleteUser(socket.id);
+        console.log("deleted user is", user);
+        if (user.length > 0) {
+          io.to(user[0].room).emit("message", {
+            user: user[0].username,
+            text: `User ${user[0].username} has left the chat.`,
+          });
+          io.to(user.room).emit("roomInfo", {
+            room: user.room,
+            users: await getUsersInRoom(user[0].room),
+          });
+        }
+      } catch (err) {
+        console.log("error while disconnecting", err);
+      }
+    });
+  });
+};
