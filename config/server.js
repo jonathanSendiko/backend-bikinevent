ada3b77b6482288b6801d598e6c3843977a5a500
Jonathan <jonathan.sendiko@gmail.com>
2021-07-02 16:00:13 +0700

    url

--- config/server.js
+++ config/server.js
@@ -1,11 +1,12 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: "https://bikinevent.id",
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
     },
-    url:"https://bikinevent.id/dashboard"
+    url: "https://bikinevent.id/dashboard",
   },
-  url:"/api"
+  url: "/api",
 });
