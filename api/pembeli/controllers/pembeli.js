5289088ebccaf4268f5a5d5045b9b64cae05b70d
Jonathan <jonathan.sendiko@gmail.com>
2021-07-10 05:43:40 +0700

    Checkout flow finished

--- api/pembeli/controllers/pembeli.js
+++ api/pembeli/controllers/pembeli.js
@@ -1,8 +1,22 @@
-'use strict';
-
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.pembeli.findOne({ id }, [
+      {
+        path: "cart",
+        populate: {
+          path: "vendor",
+        },
+      },
+    ]);
+    return sanitizeEntity(entity, { model: strapi.models.pembeli });
+  },
+};
