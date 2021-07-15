ed7e8ec08fe15520a9d941d7a7b39e9d20e8fefc
Jonathan <jonathan.sendiko@gmail.com>
2021-07-16 03:17:00 +0700

    vendor

--- api/vendor/controllers/vendor.js
+++ api/vendor/controllers/vendor.js
@@ -1,8 +1,28 @@
-'use strict';
-
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.vendor.findOne({ id }, [
+      {
+        path: "order_histories",
+        populate: {
+          path: "produks pembeli",
+          populate: {
+            path: "user",
+          },
+        },
+      },
+      {
+        path: "produks portfolios",
+      },
+    ]);
+    return sanitizeEntity(entity, { model: strapi.models.vendor });
+  },
+};
