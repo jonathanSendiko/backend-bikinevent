4b7db0a2fb11eaa59d1f75f0b1763a4ee3d6dd10
Jonathan <jonathan.sendiko@gmail.com>
2021-08-03 16:40:09 +0700

    iya

--- config/server.js
+++ config/server.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  // url: "https://bikinevent.id/api",
+  url: "https://bikinevent.id/api",
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
     },
-    // url: "https://bikinevent.id/dashboard",
+    url: "https://bikinevent.id/dashboard",
   },
 });
